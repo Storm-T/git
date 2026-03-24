df0021745505d99f65e095ee9a63583858410238
hd <haroldtsameni@gmail.com>
2026-03-24 01:03:29 +0100

    database configuration

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 // Informations d'identification
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'gituser');
+define('DB_PASSWORD', 'git@2026');
 define('DB_PORT',3306);
-define('DB_NAME', 'cc');
+define('DB_NAME', 'c2635582c_git');
  
 // Connexion a la base de donnees MySQL 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);

91a8267415b9dc19df8974619773f85cde730205
hd <haroldtsameni@gmail.com>
2026-03-24 01:43:54 +0100

    Rediriger vers le home après suppression 5

--- pages/delete.php
+++ pages/delete.php
@@ -1,6 +1,7 @@
-<?php  
-     include "../config.php";
-     
+ <!-- Footer -->
+<?php  include "../header.php" ?>
+
+<?php 
      if(isset($_GET['delete']))
      {
          $etudiantid= $_GET['delete'];
@@ -8,14 +9,9 @@
          // SQL query to delete data from user table where id = $userid
          $query = "DELETE FROM etudiants WHERE Id_Etudiant = {$etudiantid}"; 
          $delete_query= mysqli_query($conn, $query);
-         
-         if($delete_query)
-         {
-             header("Location: home.php");
-             exit();
-         }
+         header("Location: home.php");
      }
-?>
+              ?>
 
   <!-- Footer -->
-<?php include "../footer.php" ?>
+<?php include "footer.php" ?>
